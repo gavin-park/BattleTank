02d7bbf74a17c17d0306bbed210db9f67ed53cc6
DESKTOP-2VJ9KN5\zzleg <zzleggam@gmail.com>
2017-11-26 18:47:18 +0900

    Create Project

---
+++ BattleTank/Source/BattleTank/BattleTankGameModeBase.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTankGameModeBase.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnOpenDoor);
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ABattleTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	FOnOpenDoor* OnOpenDoor = nullptr;
+	
+	
+};
